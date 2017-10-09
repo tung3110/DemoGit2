9a51ab5b550c01306c74061df0239f7089922c86
Liên Thái <lienthai@Liens-Mac.local>
2017-10-09 09:31:07 +0700

    Initial Commit

---
+++ DemoGit2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoGit2
+//
+//  Created by Liên Thái on 10/9/17.
+//  Copyright © 2017 TTHD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoGit2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DemoGit2
+//
+//  Created by Liên Thái on 10/9/17.
+//  Copyright © 2017 TTHD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
